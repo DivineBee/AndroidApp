0da9319568c261946b106146beec5edc5e6226a3
Beatrix <49019844+DivineBee@users.noreply.github.com>
2021-02-21 18:55:29 +0200

    Lab2 done

---
+++ app/src/main/java/com/beatrix/mobileapplication/Expenses.kt
@@ -0,0 +1,5 @@
+package com.beatrix.mobileapplication
+
+data class Expenses(var category: String, var amount: Double, var description: String, var categoryImage: Int ) {
+
+}
--- app/src/main/java/com/beatrix/mobileapplication/SecondActivity.kt
+++ app/src/main/java/com/beatrix/mobileapplication/SecondActivity.kt
@@ -1,83 +1,47 @@
 package com.beatrix.mobileapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.beatrix.mobileapplication.databinding.ActivitySecondBinding
 import kotlinx.android.synthetic.main.activity_second.*
 
-private const val TAG = "MainActivity"
-private const val TEXT_CONTENTS = "TextContent"
+class SecondActivity : AppCompatActivity(), OnExpensesDetailsClickListener {
 
-class SecondActivity : AppCompatActivity() {
-    private var secondDescr: TextView? = null
+    lateinit var binding: ActivitySecondBinding
+    lateinit var expensesList: ArrayList<Expenses>
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "onCreate 2: called")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_second)
+        expensesList = ArrayList()
+        addExpenses()
 
-        val intent = getIntent()
-        val dateNumber = intent.getIntExtra("Number", 0)
-
-        if (dateNumber != null) {
-            val temp = dateNumber * 100
-            starNumber.text = (temp..3200).random().toString()
-        }
-        //starNumber.text = dateNumber.toString()
+        expensesRecycler.layoutManager = LinearLayoutManager(this)
+        expensesRecycler.addItemDecoration(DividerItemDecoration(this, 1))
+        expensesRecycler.adapter = ExpensesAdapter(expensesList, this)
     }
 
-    fun onBackPressed(view: View) {
-        Log.d(TAG, "onBack: called")
-        var luckyNumber = "Your lucky number is: ${starNumber.text.toString()}"
+    fun addExpenses(){
+        expensesList.add(Expenses("FOOD", 300.50, "Bought food for a week", R.drawable.food))
+        expensesList.add(Expenses("CLOTHES", 900.24, "New dress and a belt", R.drawable.clothes))
+        expensesList.add(Expenses("BEAUTY", 340.00, "Vichy cream", R.drawable.beauty))
+        expensesList.add(Expenses("BILLS", 140.50, "Paid for water and gas", R.drawable.bills))
+        expensesList.add(Expenses("CAR", 500.00, "Fueled the car", R.drawable.car))
+        expensesList.add(Expenses("DETERGENTS", 235.45, "Detergents for clothes", R.drawable.detergents))
+    }
 
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("luckyNumber", luckyNumber)
+    override fun onItemClick(item: Expenses, position: Int) {
+//        Toast.makeText(this, item.amount.toString(), Toast.LENGTH_SHORT).show()
+        var intent = Intent(this, DetailsActivity::class.java)
+        intent.putExtra("AMOUNT", item.amount.toString())
+        intent.putExtra("CATEGORY", item.category)
+        intent.putExtra("CATEGLOGO", item.categoryImage.toString())
+        intent.putExtra("DESCRIPTION", item.description)
         startActivity(intent)
-    }
-
-    override fun onStart() {
-        Log.d(TAG, "onStart 2: called")
-        super.onStart()
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        Log.d(TAG, "onRestoreInstanceState 2: called")
-        super.onRestoreInstanceState(savedInstanceState)
-        secondDescr?.text = savedInstanceState?.getString(TEXT_CONTENTS, "")
-    }
-
-    override fun onResume() {
-        Log.d(TAG, "onResume 2: called")
-        super.onResume()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "onDestroy 2: called")
-        super.onDestroy()
-    }
-
-    override fun onPause() {
-        Log.d(TAG, "onPause 2: called")
-        super.onPause()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        Log.d(TAG, "onSaveInstanceState 2: called")
-        super.onSaveInstanceState(outState)
-        outState?.putString(TEXT_CONTENTS, secondDescr?.text.toString())
-    }
-
-    override fun onStop() {
-        Log.d(TAG, "onStop 2: called")
-        super.onStop()
-    }
-
-    override fun onRestart() {
-        Log.d(TAG, "onRestart 2: called")
-        super.onRestart()
     }
 }
