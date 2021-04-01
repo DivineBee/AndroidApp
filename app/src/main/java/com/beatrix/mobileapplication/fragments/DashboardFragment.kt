690dd91eb19dccee0561df4281d619b8b4d87033
Beatrix <49019844+DivineBee@users.noreply.github.com>
2021-04-01 18:27:39 +0300

    added changes

--- app/src/main/java/com/beatrix/mobileapplication/fragments/DashboardFragment.kt
+++ app/src/main/java/com/beatrix/mobileapplication/fragments/DashboardFragment.kt
@@ -1,17 +1,19 @@
 package com.beatrix.debug.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 import com.beatrix.debug.ExchangeActivity
+import com.beatrix.debug.MainActivity
 import com.beatrix.debug.R
 import com.beatrix.debug.SecondActivity
 import kotlinx.android.synthetic.main.fragment_dashboard.*
+
 
 class DashboardFragment : Fragment() {
 
@@ -22,32 +24,16 @@ class DashboardFragment : Fragment() {
         val buttonCurrency = view.findViewById<Button>(R.id.buttonCurrency)
 
         buttonExpenses.setOnClickListener{
-            requireActivity().run {
-                val intent = Intent(this@DashboardFragment.context, SecondActivity::class.java)
-                activity?.startActivity(intent)
-            }
-//            val intent = Intent(activity, SecondActivity::class.java)
-//            startActivity(intent)
+            val intent = Intent(activity, SecondActivity::class.java)
+            startActivity(intent)
         }
 
         buttonCurrency.setOnClickListener{
-            requireActivity().run {
-                activity?.finish()
-                val intent = Intent(this@DashboardFragment.context, ExchangeActivity::class.java)
-                activity?.startActivity(intent)
-            }
-           /* val intent = Intent(activity, ExchangeActivity::class.java)
-            startActivity(intent)*/
+            val intent = Intent(activity, ExchangeActivity::class.java)
+            startActivity(intent)
         }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return view
     }
-
-    /*override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        val intent = Intent (this@DashboardFragment.context, SecondActivity::class.java)
-        startActivity(intent)
-    }*/
 
 }
